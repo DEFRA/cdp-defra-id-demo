3f36572882a2ad0406ce3201c1c62f1d30a5de95
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2024-07-16 09:41:22 +0100

    adds browserstack test

---
+++ src/defra-id-demo.e2e.js
@@ -0,0 +1,80 @@
+import { browser, expect, $ } from '@wdio/globals'
+
+import { createServer } from '~/src/server'
+import fetch from 'node-fetch'
+
+const user = {
+  userId: '86a7607c-a1e7-41e5-a0b6-a41680d05a2a',
+  email: 'some@example.com',
+  firstName: 'Test',
+  lastName: 'User',
+  loa: '1',
+  aal: '1',
+  enrolmentCount: 1,
+  enrolmentRequestCount: 1,
+  relationships: [
+    {
+      organisationName: 'Some Org',
+      relationshipRole: 'Employee',
+      roleName: 'Some role',
+      roleStatus: '1'
+    }
+  ]
+}
+
+describe('cdp-defra-id-demo', async () => {
+  const server = await createServer()
+
+  // eslint-disable-next-line no-undef
+  before(async () => {
+    // server = await createServer()
+    await server.initialize()
+    await server.start()
+  })
+
+  // eslint-disable-next-line no-undef
+  after(async () => {
+    await server.stop({ timeout: 4000 })
+  })
+
+  describe('Login via stub', () => {
+    it('Login using API created user', async () => {
+      const apiResult = await fetch(
+        'http://localhost:3200/cdp-defra-id-stub/API/register',
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(user)
+        }
+      )
+
+      expect(apiResult.status).toEqual(201)
+
+      await browser.url('/cdp-defra-id-demo')
+
+      const title = await $(`[data-testid="app-heading-title"]`)
+      await expect(title).toHaveText('Home')
+
+      // Click login
+      const loginLink = await $('#login-link')
+      await expect(loginLink).toHaveText('here')
+      await loginLink.click()
+
+      // Select profile and login
+      await expect(browser).toHaveTitle('DEFRA ID Login | cdp-defra-id-stub')
+      const profileLink = await $('=Log in')
+      await expect(profileLink).toExist()
+      await profileLink.click()
+
+      // Check we're logged in
+      await expect($('strong=Test User')).toExist()
+      await expect($('code=some@example.com')).toExist()
+
+      // Log out
+      await $('=Sign out').click()
+      const titleAfterSignOut = await $(`[data-testid="app-heading-title"]`)
+      await expect(titleAfterSignOut).toHaveText('Home')
+      await expect($('strong=Test User')).not.toExist()
+    })
+  })
+})
