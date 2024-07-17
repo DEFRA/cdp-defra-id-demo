642c473d161f290b6cab1bbc6c0054a83e802205
Christopher Turner <23338096+christopherjturner@users.noreply.github.com>
2024-07-17 15:37:01 +0100

    adds browserstack test (#12)
    adds browserstack test

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   extends: ['stylelint-config-gds/scss'],
-  ignoreFiles: ['**/public/**', '**/package/**', '**/vendor/**']
+  ignoreFiles: [
+    '**/public/**',
+    '**/package/**',
+    '**/vendor/**',
+    '**/allure-results/**',
+    '**/allure-report/**'
+  ]
 }
